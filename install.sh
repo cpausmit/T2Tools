f6aa513fc661fb099fd24932aaacac71f0604874
Christoph Paus <paus@mit.edu>
2016-05-02 21:39:21 -0400

    Unified set of variable names

--- install.sh
+++ install.sh
@@ -13,13 +13,14 @@ touch setup.sh
 
 # first the base directory and the path
 echo "# CAREFUL THIS FILE IS GENERATED AT INSTALL"                 >> setup.sh
-echo "export TICKET_HOLDER=$TICKET_HOLDER"                         >> setup.sh
-echo "export TIER2_USER=$TIER2_USER"                               >> setup.sh
-echo "export T2TOOLS_BASE=`pwd`"                                   >> setup.sh
 echo "export T2TOOLS_SERVER=t2bat0380.cmsaf.mit.edu"               >> setup.sh
+echo "export T2TOOLS_TICKET=$TICKET_HOLDER"                        >> setup.sh
+echo "export T2TOOLS_USER=$TIER2_USER"                             >> setup.sh
+echo "export T2TOOLS_BASE=`pwd`"                                   >> setup.sh
 echo "export PATH=\"\${PATH}:\${T2TOOLS_BASE}/bin\""               >> setup.sh
 echo "export PYTHONPATH=\"\${PYTONPATH}:\${T2TOOLS_BASE}/python\"" >> setup.sh
 echo ""                                                            >> setup.sh
+echo "# reduce memory used on hdfs commands"                       >> setup.sh
 echo "export HADOOP_CLIENT_OPTS=-Xmx32m"                           >> setup.sh
 
 exit 0
